36e83fbdee1335ea78490eeca60e57d95a105134
Michael Bar-Sinai <mich.barsinai@gmail.com>
2020-05-20 01:08:28 +0300

    Added Instagram contact option (closes #92)

--- code/app/models/Platform.scala
+++ code/app/models/Platform.scala
@@ -2,7 +2,7 @@ package models
 
 object Platform extends Enumeration {
   type Platform = Value
-  val Phone, Email, Mail, Fax, Facebook, Twitter = Value
+  val Phone, Email, Mail, Fax, Facebook, Twitter, Instagram = Value
   
   def tryConvert(name:String):Option[Platform.Value] = {
     try {
