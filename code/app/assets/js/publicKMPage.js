f6a335cf22d0eb1da5c1baa356d3d9d4c0c4b888
Michael Bar-Sinai <mich.barsinai@gmail.com>
2020-06-23 13:09:45 +0300

    Users approve EULA before sending their first KM interaction (closes #114)

--- code/app/assets/js/publicKMPage.js
+++ code/app/assets/js/publicKMPage.js
@@ -1,13 +1,13 @@
 /* jshint esversion:6 */
-
 const PublicKMPage = (function(){
     const EULA_ACCEPTANCE_KEY = "EULA_ACCEPTANCE_KEY";
-
-    let nextUrl;
+    let pendingAction;
+    let campaignId, kmId;
 
     function ensureEulaAcceptance() {
         if (!document.cookie.split('; ').find(row => row.startsWith(EULA_ACCEPTANCE_KEY))) {
             $("#eulaApprovalModal").modal("show");
+            return false;
         } else {
             return true;
         }
@@ -15,16 +15,22 @@ const PublicKMPage = (function(){
 
     function approveEula() {
         document.cookie = "EULA_ACCEPTANCE_KEY=true; expires=Fri, 31 Dec 9999 23:59:59 GMT";
-        $("#eulaApprovalModal").modal("close");
+        $("#eulaApprovalModal").modal("hide");
         perform();
     }
 
     function perform() {
-        document.location.href=nextUrl;
+        // TODO report usage
+        if ( pendingAction.url.indexOf("http")=== 0 ) {
+            const win = window.open(pendingAction.url, '_blank');
+            win.focus();
+        } else {
+            document.location.href=decodeURI(pendingAction.url);
+        }
     }
 
-    function contactKm( url ) {
-        nextUrl = url;
+    function contactKm( medium, url ) {
+        pendingAction = {medium:medium, url:url};
         if ( ensureEulaAcceptance() ) {
             perform();
         }
@@ -32,7 +38,8 @@ const PublicKMPage = (function(){
 
     return {
         contactKm:contactKm,
-        approveEula:approveEula
-
+        approveEula:approveEula,
+        setKm: function(aKM){ kmId = aKM; },
+        setCampaign: function(aCampaign){ campaignId = aCampaign; }
     };
 })();
